364e713e84c4984ba20f7c3e7f6f295eb114686b
lu <1769982399@qq.com>
2022-10-20 23:21:12 +0800

    完善test_log.cpp

--- tests/test_log.cpp
+++ tests/test_log.cpp
@@ -1,15 +1,18 @@
 #include <Utils/log.h>
 #include <chrono>
 #include <memory>
+#include <string>
 #include <thread>
 
 int main() {
   LOG_DEFAULT;
-  // loop::Logger::Instance().add_channel(std::make_shared<loop::FileChannel>());
+  loop::Logger::Instance().add_channel(std::make_shared<loop::FileChannel>());
   loop::Logger::Instance().set_async();
-  TraceL << 1;
-  DebugL << 2;
-  InfoL << 3;
-  WarnL << 4;
-  ErrorL << 5;
+  TraceL << "第一条log";
+  DebugL << 12.345;
+  InfoL << "sleep for 2 seconds";
+  std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+  std::string warn("This is a warning");
+  WarnL << warn;
+  ErrorL << '!';
 }
