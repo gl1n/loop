98f5fb7ceb8426c3c33b24249da3a2d001a1c527
lu <1769982399@qq.com>
2022-10-28 22:46:35 +0800

    完成test_semaphore2的debug

--- src/util.cpp
+++ src/util.cpp
@@ -3,8 +3,12 @@
 #include <chrono>
 #include <sys/types.h>
 
+namespace loop {
+
 u_int64_t timeSinceEpochMillisecs() {
   return std::chrono::duration_cast<std::chrono::milliseconds>(
              std::chrono::system_clock::now().time_since_epoch())
       .count();
 }
+
+} // namespace loop
--- tests/test_semaphore.cpp
+++ tests/test_semaphore.cpp
@@ -1,49 +1,63 @@
-#include <Thread/semaphore.h>
-#include <Utils/log.h>
-#include <chrono>
-#include <mutex>
-#include <queue>
-#include <thread>
-#include <vector>
+#include "Thread/semaphore.h"
+#include "Thread/threadgroup.h"
+#include "Utils/TimeTicker.h"
+#include "Utils/log.h"
+#include <atomic>
+#include <cstddef>
+
+constexpr int MAX_TASK_SIZE = 1000 * 10000;
 
 loop::Semaphore g_sem;
-std::mutex g_mtx;
+std::atomic_llong g_produced(0);
+std::atomic_llong g_consumed(0);
+
+//生产者线程
+void onProduce() {
+  while (true) {
+    if (g_produced >= MAX_TASK_SIZE) {
+      break;
+    }
+    g_produced++;
+    g_sem.post();
+  }
+}
+
+//消费者线程
+void onConsume() {
+  while (true) {
+    g_sem.wait();
+    g_consumed++;
+    if (g_consumed > g_produced) {
+      ErrorL << g_consumed << " > " << g_produced;
+    }
+  }
+}
 
 int main() {
   LOG_DEFAULT;
 
-  std::queue<int> q;
+  loop::Ticker ticker;
+  loop::ThreadGroup producers;
+  for (size_t i = 0; i < std::thread::hardware_concurrency(); ++i) {
+    producers.create_thread([]() { onProduce(); });
+  }
 
-  std::vector<std::thread> threads;
+  loop::ThreadGroup consumers;
+  for (size_t i = 0; i < 4; i++) {
+    consumers.create_thread([]() { onConsume(); });
+  }
 
-  int num = 4;
-  for (int i = 0; i < num; i++) {
-    threads.emplace_back([&q, i]() {
-      while (true) {
-        {
-          std::lock_guard<std::mutex> lg(g_mtx);
-          q.push(i + 1);
-          InfoL << "Produce item: " << q.back();
-        }
-        g_sem.post();
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-      }
-    });
+  producers.join_all();
+
+  DebugL << "生产者线程退出，耗时：" << ticker.sinceLast() << "ms,"
+         << "生产任务数：" << g_produced << "，消费任务数" << g_consumed;
+
+  int i = 5;
+  while (i--) {
+    DebugL << "程序退出倒计时：" << i << "消费任务数：" << g_consumed;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
   }
-  for (int i = 0; i < num; i++) {
-    threads.emplace_back([&q]() {
-      while (true) {
-        g_sem.wait();
-        {
-          std::lock_guard<std::mutex> lg(g_mtx);
-          InfoL << "Consume item: " << q.front();
-          q.pop();
-        }
-      }
-    });
-  }
-  //必须join或者detach，因为main函数退出时所有std::thread对象都会被析构，如果这些对象是joinable，则会调用terminate
-  for (auto &th : threads) {
-    th.join();
-  }
+
+  WarnL << "强制关闭线程，可能会发生coredump";
+  return 0;
 }
