d85c68440f195a2daf724753716b51a29d34426e
lu <1769982399@qq.com>
2022-11-02 22:23:31 +0800

    添加uncopyable类

--- include/Utils/util.h
+++ include/Utils/util.h
@@ -4,4 +4,20 @@
 namespace loop {
 //获取1970年至今毫秒数
 u_int64_t timeSinceEpochMillisecs();
+
+// 禁止拷贝基类。如果子类显示定义了自己的复制函数的话还是能够进行复制的。
+class uncopyable {
+  // public的话noncopyable能够实例化，private的话子类无法实例化
+protected:
+  uncopyable() {}
+  ~uncopyable() {}
+
+private:
+  //禁止拷贝
+  uncopyable(const uncopyable &) = delete;
+  uncopyable &operator=(const uncopyable &) = delete;
+  uncopyable(uncopyable &&) = delete;
+  uncopyable &operator=(uncopyable &&) = delete;
+};
+
 } // namespace loop
